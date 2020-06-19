fa8a99949cc248d33f19d148561b4b75a4d1611b
Daniel Victor Ferrari <danielvictorferrari@gmail.com>
2020-06-19 14:27:16 -0300

    chore: adicionado if para erro de IO

--- cartao/src/main/java/br/com/mastertech/cartao/client/ClienteClientFallback.java
+++ cartao/src/main/java/br/com/mastertech/cartao/client/ClienteClientFallback.java
@@ -2,6 +2,8 @@ package br.com.mastertech.cartao.client;
 
 import br.com.mastertech.cartao.dtos.Cliente;
 
+import java.io.IOException;
+
 public class ClienteClientFallback implements ClienteClient {
     private Exception cause;
 
@@ -11,6 +13,13 @@ public class ClienteClientFallback implements ClienteClient {
 
     @Override
     public Cliente getById(Integer id) {
+        if (cause instanceof IOException) {
+            throw new RuntimeException("Serviço cliente fora do ar");
+        }
+
+        System.out.println(cause.getLocalizedMessage());
+        System.out.println(cause.getClass().getName());
+        System.out.println(cause.getCause());
         return new Cliente();
     }
 }
